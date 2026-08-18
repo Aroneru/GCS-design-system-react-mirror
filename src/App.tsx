9c13f50628ec29b0741179bf51e72aa3ac50f191
keisharmdh <keisha_ramadhani@apps.ipb.ac.id>
2026-08-18 11:56:36 +0700

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,6 @@ import { CheckboxPage } from "./docs/pages/form/CheckboxPage";
 import { TextAreaUsulanPage } from "./docs/pages/form/input-field/TextAreaUsulanPage";
 import { PlaceholderPage } from "./docs/pages/PlaceholderPage";
 
-<<<<<<< HEAD
 import { ComponentsOverview } from "./docs/pages/components/ComponentsOverview";
 import { ContainerPage } from "./docs/pages/components/ContainerPage";
 import { ButtonPage } from "./docs/pages/components/ButtonPage";
@@ -34,18 +33,8 @@ import { BadgePage } from "./docs/pages/components/BadgePage";
 import { AlertPage } from "./docs/pages/components/AlertPage";
 import { ToastPage } from "./docs/pages/components/ToastPage";
 import { CardPage } from "./docs/pages/components/CardPage";
+import { HeroPage } from "./docs/pages/components/HeroPage";
 import { FooterPage } from "./docs/pages/components/FooterPage";
-=======
-import { ComponentsOverview } from './docs/pages/components/ComponentsOverview'
-import { ContainerPage } from './docs/pages/components/ContainerPage'
-import { ButtonPage } from './docs/pages/components/ButtonPage'
-import { BadgePage } from './docs/pages/components/BadgePage'
-import { AlertPage } from './docs/pages/components/AlertPage'
-import { ToastPage } from './docs/pages/components/ToastPage'
-import { CardPage } from './docs/pages/components/CardPage'
-import { HeroPage } from './docs/pages/components/HeroPage'
-import { FooterPage } from './docs/pages/components/FooterPage'
->>>>>>> f949205f7c484a53b164e442b85d83bbf9fc95d7
 import {
   NavbarDesktopPreview,
   NavbarMobilePreview,
@@ -53,7 +42,6 @@ import {
 } from "./docs/pages/components/NavbarPage";
 import { BreadcrumbPage } from "./docs/pages/components/BreadcrumbPage";
 import { PaginationPage } from "./docs/pages/components/PaginationPage";
-// import { PaginationPage } from "./docs/pages/components/PaginationPage";
 
 const routes: Record<string, () => React.ReactElement> = {
   "/": HomePage,
